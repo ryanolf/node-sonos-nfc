c3a78dd8277256436dfb3b96bd2576c57b4c4507
Ryan Olf <ryan@nanoprecisionmedical.com>
2021-01-02 22:48:12 -0800

    Refactor Sonos command into module

--- sonos_nfc.js
+++ sonos_nfc.js
@@ -1,10 +1,6 @@
 import {NFC} from 'nfc-pcsc'
-import ndef from 'nfccard-tool'
-const nfcCard = ndef
-import fetch from 'node-fetch'
-import fs from 'fs'
-
-var {sonos_room, sonos_http_api} = JSON.parse(fs.readFileSync('usersettings.json', 'utf-8'))
+import {default as nfcCard} from 'nfccard-tool'
+import process_sonos_command from './lib/process_sonos_command.js'
 
 const nfc = new NFC()
 
@@ -58,71 +54,9 @@ nfc.on('reader', reader => {
                         case 'text':
                             const received_text = record.text.toLowerCase()
                             console.log('Read from NFC tag with message: ', received_text)
-                            
-                            if (received_text.startsWith('apple:')) {
-                                service_type = "applemusic"
-                                sonos_instruction = "applemusic/now/" + received_text.slice(6)
-                            
-                            } else if (received_text.startsWith('applemusic:')) {
-                                service_type = "applemusic"
-                                sonos_instruction = "applemusic/now/" + received_text.slice(11)
 
-                            } else if (received_text.startsWith('http')) {
-                                service_type = "completeurl"
-                                sonos_instruction = received_text
-                           
-                            } else if (received_text.startsWith('spotify:')) {
-                                service_type = "spotify"
-                                sonos_instruction = "spotify/now/" + received_text
-
-                            } else if (received_text.startsWith('tunein:')) {
-                                service_type = "tunein"
-                                sonos_instruction = "tunein/now/" + received_text
-
-                            } else if (received_text.startsWith('amazonmusic:')) {
-                                service_type = "amazonmusic"
-                                sonos_instruction = "amazonmusic/now/" + received_text.slice(12)
-
-                            } else if (received_text.startsWith('command:')) {
-                                service_type = "command"
-                                sonos_instruction = received_text.slice(8)
-
-                            } else if (received_text.startsWith('room:')) {
-                                sonos_room = received_text.slice(5)
-                                console.log("Sonos room changed to %s", sonos_room)
-                                return
-                            }  
+                            await process_sonos_command(received_text)
                     }
-
-                    if (!service_type) {
-                        console.log("Service type not recognised. NFC tag text should begin 'spotify', 'tunein', 'amazonmusic', 'apple'/'applemusic', 'command' or 'room'.")
-                        return
-                    }
-
-                    console.log("Detected '%s' service request", service_type)
-
-                    if (service_type != "command") {
-                        console.log("Clearing Sonos queue")
-                        const res = await fetch(sonos_http_api + "/" + sonos_room + "/clearqueue")
-                        if (!res.ok) throw new Error(`Unexpected response while clearing queue: ${res.status}`)
-                    }
-
-                    let urltoget
-                    if (service_type == "completeurl") {
-                        urltoget = sonos_instruction
-                    } else {
-                        urltoget = sonos_http_api + "/" + sonos_room + "/" + sonos_instruction
-                    }
-                    
-                    // Perform the requested action on the sonos API
-                    console.log("Fetching URL via HTTP: %s", urltoget)
-                    const res = await fetch(urltoget)
-                    if (!res.ok) throw new Error(`Unexpected response while sending instruction: ${res.status}`)
-                    console.log("Sonos API reports: ", await res.json())
-                    
-                    // Wait a second before processing nexg record so the API has time to respond to first command
-                    // e.g. want to seek on a new queue -- need the new queue to exist
-                    await new Promise(resolve => setTimeout(resolve, 1000));
                 }
       
             } else {
@@ -130,9 +64,8 @@ nfc.on('reader', reader => {
             }
             
         } catch (err) {
-            console.error(`error when reading data or communicating with sonos API`, err)
-        }
-      
+            console.error(err.toString())
+        }   
     })
     
     reader.on('card.off', card => {	
