97e2d3792486673ba452b77cfdc30590603b4f08
Mattijs Bliek <mattijs@mollie.com>
2021-07-31 15:08:42 +0200

    Add Prettier

--- sonos_nfc.js
+++ sonos_nfc.js
@@ -1,87 +1,100 @@
-import {NFC} from 'nfc-pcsc'
-import {default as nfcCard} from 'nfccard-tool'
-import process_sonos_command from './lib/process_sonos_command.js'
+import { NFC } from 'nfc-pcsc';
+import { default as nfcCard } from 'nfccard-tool';
+import process_sonos_command from './lib/process_sonos_command.js';
 
-const nfc = new NFC()
+const nfc = new NFC();
 
-console.log("Control your Sonos with NFC cards. Searching for PCSC-compatible NFC reader devices...")
+console.log(
+  'Control your Sonos with NFC cards. Searching for PCSC-compatible NFC reader devices...'
+);
 
-nfc.on('reader', reader => {
+nfc.on('reader', (reader) => {
+  console.log(`${reader.reader.name} device attached`);
 
-    console.log(`${reader.reader.name} device attached`)
+  reader.on('card', async (card) => {
+    // card is object containing following data
+    // String standard: TAG_ISO_14443_3 (standard nfc tags like MIFARE Ultralight) or TAG_ISO_14443_4 (Android HCE and others)
+    // String type: same as standard
+    // Buffer atr
 
-    reader.on('card', async card => {
+    console.log(
+      `${reader.reader.name} detected %s with UID %s`,
+      card.type,
+      card.uid
+    );
 
-        // card is object containing following data
-        // String standard: TAG_ISO_14443_3 (standard nfc tags like MIFARE Ultralight) or TAG_ISO_14443_4 (Android HCE and others)
-        // String type: same as standard
-        // Buffer atr
+    try {
+      /**
+       *  1 - READ HEADER
+       *  Read from block 0 to block 4 (20 bytes length) in order to parse tag information
+       *  Block 4 is the first data block -- should have the TLV info
+       */
+      const cardHeader = await reader.read(0, 20);
 
-        console.log(`${reader.reader.name} detected %s with UID %s`, card.type, card.uid)
+      const tag = nfcCard.parseInfo(cardHeader);
+      // console.log('tag info:', JSON.stringify(tag))
 
-        try {
-            /**
-             *  1 - READ HEADER
-             *  Read from block 0 to block 4 (20 bytes length) in order to parse tag information
-             *  Block 4 is the first data block -- should have the TLV info
-             */
-            const cardHeader = await reader.read(0, 20)
-      
-            const tag = nfcCard.parseInfo(cardHeader)
-            // console.log('tag info:', JSON.stringify(tag))
-      
-            /**
-             *  2 - Read the NDEF message and parse it if it's supposed there is one
-             *  The NDEF message must begin in block 4 -- no locked bits, etc.
-             *  Make sure cards are initialized before writing.
-             */
+      /**
+       *  2 - Read the NDEF message and parse it if it's supposed there is one
+       *  The NDEF message must begin in block 4 -- no locked bits, etc.
+       *  Make sure cards are initialized before writing.
+       */
 
-            if(nfcCard.isFormatedAsNDEF() && nfcCard.hasReadPermissions() && nfcCard.hasNDEFMessage()) {
-      
-                // Read the appropriate length to get the NDEF message as buffer
-                const NDEFRawMessage = await reader.read(4, nfcCard.getNDEFMessageLengthToRead()) // starts reading in block 0 until end
-      
-                // Parse the buffer as a NDEF raw message
-                const NDEFMessage = nfcCard.parseNDEF(NDEFRawMessage)
-      
-                // console.log('NDEFMessage:', NDEFMessage)
-                
-                for (const record of NDEFMessage) {
-                    let service_type, sonos_instruction
-                    switch (record.type) {
-                        case 'uri':
-                            record.text = record.uri
-                        case 'text':
-                            const received_text = record.text
-                            console.log('Read from NFC tag with message: ', received_text)
+      if (
+        nfcCard.isFormatedAsNDEF() &&
+        nfcCard.hasReadPermissions() &&
+        nfcCard.hasNDEFMessage()
+      ) {
+        // Read the appropriate length to get the NDEF message as buffer
+        const NDEFRawMessage = await reader.read(
+          4,
+          nfcCard.getNDEFMessageLengthToRead()
+        ); // starts reading in block 0 until end
 
-                            await process_sonos_command(received_text)
-                    }
-                }
-      
-            } else {
-              console.log('Could not parse anything from this tag: \n The tag is either empty, locked, has a wrong NDEF format or is unreadable.')
-            }
-            
-        } catch (err) {
-            console.error(err.toString())
-        }   
-    })
-    
-    reader.on('card.off', card => {	
-        console.log(`${reader.reader.name}: %s with UID %s removed`, card.type, card.uid)
-    })
+        // Parse the buffer as a NDEF raw message
+        const NDEFMessage = nfcCard.parseNDEF(NDEFRawMessage);
 
-    reader.on('error', err => {
-        console.log(`${reader.reader.name} an error occurred`, err)
-    })
+        // console.log('NDEFMessage:', NDEFMessage)
 
-    reader.on('end', () => {
-        console.log(`${reader.reader.name}  device removed`)
-    })
+        for (const record of NDEFMessage) {
+          let service_type, sonos_instruction;
+          switch (record.type) {
+            case 'uri':
+              record.text = record.uri;
+            case 'text':
+              const received_text = record.text;
+              console.log('Read from NFC tag with message: ', received_text);
 
-})
+              await process_sonos_command(received_text);
+          }
+        }
+      } else {
+        console.log(
+          'Could not parse anything from this tag: \n The tag is either empty, locked, has a wrong NDEF format or is unreadable.'
+        );
+      }
+    } catch (err) {
+      console.error(err.toString());
+    }
+  });
 
-nfc.on('error', err => {
-    console.log('an NFC error occurred', err)
-})
+  reader.on('card.off', (card) => {
+    console.log(
+      `${reader.reader.name}: %s with UID %s removed`,
+      card.type,
+      card.uid
+    );
+  });
+
+  reader.on('error', (err) => {
+    console.log(`${reader.reader.name} an error occurred`, err);
+  });
+
+  reader.on('end', () => {
+    console.log(`${reader.reader.name}  device removed`);
+  });
+});
+
+nfc.on('error', (err) => {
+  console.log('an NFC error occurred', err);
+});
